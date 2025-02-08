7e6922d9ba925a14b541151412b5618ce092d1e4
Can Nguyen <cangod05@gmail.com>
2025-02-08 16:26:45 +0700

    chore: add css module

--- components/main-header.js
+++ components/main-header.js
@@ -1,15 +1,16 @@
-import Link from 'next/link';
 import logoImg from '@/assets/logo.png';
+import Link from 'next/link';
 import Image from 'next/image';
+import classes from './main-header.module.css';
 
 const MainHeader = () => {
   return (
-    <header>
-      <Link href="/">
-        <Image src={logoImg} alt="A plate with food on it" />
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
+        <Image src={logoImg} alt="A plate with food on it" priority />
         NextLevel Food
       </Link>
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
