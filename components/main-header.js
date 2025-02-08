4cd6ca5332181f1cdd43daec984427596f3327bf
ngohoai234 <cangod05@gmail.com>
2025-02-08 11:31:05 +0700

    chore: add main header

---
+++ components/main-header.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import logoImg from '@/assets/logo.png';
+import Image from 'next/image';
+
+const MainHeader = () => {
+  return (
+    <header>
+      <Link href="/">
+        <Image src={logoImg} alt="A plate with food on it" />
+        NextLevel Food
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
