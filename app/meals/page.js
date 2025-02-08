87428ce471a7cfc28286ba08711be4473a8bd215
ngohoai234 <cangod05@gmail.com>
2025-02-08 22:34:18 +0700

    chore: add loading meals show all except slow data

--- app/meals/page.js
+++ app/meals/page.js
@@ -3,10 +3,15 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import MealGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+import MealsLoadingPage from './loading-out';
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealGrid meals={meals} />;
+}
 
 export default async function MealsPage() {
-  const meals = await getMeals();
-
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +25,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealGrid meals={meals} />
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
