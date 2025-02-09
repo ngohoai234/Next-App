9418285407da7a4bd454887cd958b6d8f4f196ab
ngohoai234 <cangod05@gmail.com>
2025-02-09 09:25:11 +0700

    chore: add fetch meal detail

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,32 @@
-export default function MealDetailsPage() {
-  return <h1>Meal Details</h1>;
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+export default async function MealDetailsPage({ params }) {
+  const meal = await getMeal(params.mealSlug);
+  const instructions = meal.instructions.replace(/\n/g, '<br/>');
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+      </header>
+      <div className={classes.headerText}>
+        <h1>{meal.title}</h1>
+        <p className={classes.creator}>
+          by <a href={`mailto:${'EMAIL'}`}>{meal.creator_email}</a>
+        </p>
+        <p className={classes.summary}>{meal.summary}</p>
+      </div>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
